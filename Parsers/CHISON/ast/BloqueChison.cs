cc7ad8e40ff29818273a1849afccc98abbe796fa
Oscar Morales <oskralberto14@gmail.com>
2019-09-26 01:35:38 -0600

    Terminando rollback

--- Parsers/CHISON/ast/BloqueChison.cs
+++ Parsers/CHISON/ast/BloqueChison.cs
@@ -5,6 +5,7 @@ using GramaticasCQL.Parsers.CQL.ast.expresion;
 using GramaticasCQL.Parsers.CQL.ast.instruccion;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -74,6 +75,8 @@ namespace GramaticasCQL.Parsers.CHISON.ast
                                 {
                                     if (lista.Valores != null)
                                     {
+                                        LeerArchivo(lista, e, log, errores);
+
                                         foreach (Expresion parm in lista.Valores)
                                         {
                                             if (parm is BloqueChison bloque)
@@ -144,6 +147,8 @@ namespace GramaticasCQL.Parsers.CHISON.ast
                                 {
                                     if (lista.Valores != null)
                                     {
+                                        LeerArchivo(lista, e, log, errores);
+
                                         foreach (Expresion expr in lista.Valores)
                                         {
                                             object obj;
@@ -243,6 +248,8 @@ namespace GramaticasCQL.Parsers.CHISON.ast
                                     attrs = true;
                                     if (lista.Valores != null)
                                     {
+                                        LeerArchivo(lista, e, log, errores);
+
                                         foreach (Expresion expr in lista.Valores)
                                         {
                                             if (expr is BloqueChison bloque)
@@ -364,6 +371,8 @@ namespace GramaticasCQL.Parsers.CHISON.ast
                                 {
                                     if (lista.Valores != null)
                                     {
+                                        LeerArchivo(lista, e, log, errores);
+
                                         foreach (Expresion expr in lista.Valores)
                                         {
                                             if (expr is BloqueChison bloque)
@@ -542,6 +551,8 @@ namespace GramaticasCQL.Parsers.CHISON.ast
                                 {
                                     if (lista.Valores != null)
                                     {
+                                        LeerArchivo(lista, e, log, errores);
+
                                         foreach (Expresion expr in lista.Valores)
                                         {
                                             if (expr is BloqueChison bloque)
@@ -597,6 +608,8 @@ namespace GramaticasCQL.Parsers.CHISON.ast
                                 {
                                     if (lista.Valores != null)
                                     {
+                                        LeerArchivo(lista, e, log, errores);
+
                                         foreach (Expresion expr in lista.Valores)
                                         {
                                             if (expr is BloqueChison bloque)
@@ -992,6 +1005,61 @@ namespace GramaticasCQL.Parsers.CHISON.ast
                     break;
             }
             return null;
+        }
+
+        public void LeerArchivo(Lista lista, Entorno e, LinkedList<Salida> log, LinkedList<Error> errores)
+        {
+            if (lista.Valores.Count() > 0)
+            {
+                Expresion val = lista.Valores.ElementAt(0);
+
+                if (val.Tipo.IsVoid())
+                {
+                    string archivo = val.GetValor(e, log, errores).ToString().Replace("$", "").Replace("{", "").Replace("}", "");
+
+                    StreamReader reader = null;
+                    try
+                    {
+                        if (File.Exists(archivo))
+                        {
+                            reader = new StreamReader(archivo);
+                            string cadena = reader.ReadToEnd();
+
+                            AnalizadorCHISON chison = new AnalizadorCHISON();
+
+                            if (chison.AnalizarEntrada(cadena))
+                            {
+                                object obj = chison.GenerarArbol(chison.Raiz.Root);
+
+                                if (obj != null)
+                                {
+                                    if (obj is ASTCHISON ast)
+                                    {
+                                        if (ast.Bloques != null)
+                                        {
+                                            lista.Valores = new LinkedList<Expresion>();
+                                            foreach (BloqueChison b in ast.Bloques)
+                                            {
+                                                lista.Valores.AddLast((Expresion)b);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Excepción BloqueChison: " + ex.Message);
+                    }
+                    finally
+                    {
+                        if (reader != null)
+                            reader.Close();
+                    }
+                }
+            }
         }
     }
 
--- Parsers/CHISON/ast/Users.cs
+++ Parsers/CHISON/ast/Users.cs
@@ -3,6 +3,7 @@ using GramaticasCQL.Parsers.CQL.ast.expresion;
 using GramaticasCQL.Parsers.CQL.ast.instruccion;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,8 @@ namespace GramaticasCQL.Parsers.CHISON.ast
             {
                 if (lista.Valores != null)
                 {
+                    LeerArchivo(lista, e, log, errores);
+
                     foreach (Expresion expr in lista.Valores)
                     {
                         if (expr is BloqueChison bloque)
@@ -55,6 +58,61 @@ namespace GramaticasCQL.Parsers.CHISON.ast
             else
                 return null; //debe ser una lista
             return null;
+        }
+
+        public void LeerArchivo(Lista lista, Entorno e, LinkedList<Salida> log, LinkedList<Error> errores)
+        {
+            if (lista.Valores.Count() > 0)
+            {
+                Expresion val = lista.Valores.ElementAt(0);
+
+                if (val.Tipo.IsVoid())
+                {
+                    string archivo = val.GetValor(e, log, errores).ToString().Replace("$", "").Replace("{", "").Replace("}", "");
+
+                    StreamReader reader = null;
+                    try
+                    {
+                        if (File.Exists(archivo))
+                        {
+                            reader = new StreamReader(archivo);
+                            string cadena = reader.ReadToEnd();
+
+                            AnalizadorCHISON chison = new AnalizadorCHISON();
+
+                            if (chison.AnalizarEntrada(cadena))
+                            {
+                                object obj = chison.GenerarArbol(chison.Raiz.Root);
+
+                                if (obj != null)
+                                {
+                                    if (obj is ASTCHISON ast)
+                                    {
+                                        if (ast.Bloques != null)
+                                        {
+                                            lista.Valores = new LinkedList<Expresion>();
+                                            foreach (BloqueChison b in ast.Bloques)
+                                            {
+                                                lista.Valores.AddLast((Expresion)b);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Excepción BloqueChison: " + ex.Message);
+                    }
+                    finally
+                    {
+                        if (reader != null)
+                            reader.Close();
+                    }
+                }
+            }
         }
     }
 }
